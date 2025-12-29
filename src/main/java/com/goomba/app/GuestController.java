689cac5baf8847f9ebdb2096f319e2308740e551
richardr <richardareid2002@googlemail.com>
2025-12-29 18:47:58 +0000

    Added login name to response.

--- src/main/java/com/goomba/app/GuestController.java
+++ src/main/java/com/goomba/app/GuestController.java
@@ -1,5 +1,7 @@
 package com.goomba.app;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/guest")
 public class GuestController {
     @GetMapping
-    public String guest() {
-        return "Hey there guest...";
+    public String guest(@AuthenticationPrincipal UserDetails userDetails) {
+        return "Hey there guest... " + userDetails.getUsername();
     }
 }
--- src/main/java/com/goomba/app/RestrictedController.java
+++ src/main/java/com/goomba/app/RestrictedController.java
@@ -1,5 +1,7 @@
 package com.goomba.app;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestrictedController {
     @GetMapping
     public String accessible() {
-        return "You have access...";
+        var name = SecurityContextHolder.getContext().getAuthentication().getName();
+        return "You have access... " + name;
     }
 }
